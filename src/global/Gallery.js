58355031bfc663d2164fda1ddb267fa3b67be1f1
RanR112 <146650544+RanR112@users.noreply.github.com>
2025-03-06 21:33:14 +0700

    add img to local

--- src/global/Gallery.js
+++ src/global/Gallery.js
@@ -1,179 +1,46 @@
+import { 
+    Gallery1, Gallery2, Gallery3, Gallery4, Gallery5, Gallery6, Gallery7, Gallery8, Gallery9, Gallery10, 
+    Gallery11, Gallery12, Gallery13, Gallery14, Gallery15, Gallery16, Gallery17, Gallery18, Gallery19, Gallery20, 
+    Gallery21, Gallery22, Gallery23, Gallery24, Gallery25, Gallery26, Gallery27, Gallery28, Gallery29, Gallery30, 
+    Gallery31, Gallery32, Gallery33, Gallery34, Gallery35 
+} from '../assets';
+
 const GalleryData = [
-    {
-        "id": 1,
-        "img": "https://i.imgur.com/mTuj5Ou.jpeg",
-        "alt": "Gallery 1"
-    },
-    {
-        "id": 2,
-        "img": "https://i.imgur.com/ye4w45S.jpeg",
-        "alt": "Gallery 2"
-    },
-    {
-        "id": 3,
-        "img": "https://i.imgur.com/JZTr1Ks.jpeg",
-        "alt": "Gallery 3"
-    },
-    {
-        "id": 4,
-        "img": "https://i.imgur.com/4rLerLR.jpeg",
-        "alt": "Gallery 4"
-    },
-    {
-        "id": 5,
-        "img": "https://i.imgur.com/A3IkJJc.jpeg",
-        "alt": "Gallery 5"
-    },
-    {
-        "id": 6,
-        "img": "https://i.imgur.com/yJNbMab.jpeg",
-        "alt": "Gallery 6"
-    },
-    {
-        "id": 7,
-        "img": "https://i.imgur.com/27vCmSv.jpeg",
-        "alt": "Gallery 7"
-    },
-    {
-        "id": 8,
-        "img": "https://i.imgur.com/hwqtXuR.jpeg",
-        "alt": "Gallery 8"
-    },
-    {
-        "id": 9,
-        "img": "https://i.imgur.com/Nhke1Sf.jpeg",
-        "alt": "Gallery 9"
-    },
-    {
-        "id": 10,
-        "img": "https://i.imgur.com/ps1VT0O.jpeg",
-        "alt": "Gallery 10"
-    },
-    {
-        "id": 11,
-        "img": "https://i.imgur.com/wiPrV2N.jpeg",
-        "alt": "Gallery 11"
-    },
-    {
-        "id": 12,
-        "img": "https://i.imgur.com/O06qtOA.jpeg",
-        "alt": "Gallery 12"
-    },
-    {
-        "id": 13,
-        "img": "https://i.imgur.com/SSVsCIQ.jpeg",
-        "alt": "Gallery 13"
-    },
-    {
-        "id": 14,
-        "img": "https://i.imgur.com/rWaJCfw.jpeg",
-        "alt": "Gallery 14"
-    },
-    {
-        "id": 15,
-        "img": "https://i.imgur.com/AwZ7YJ3.jpeg",
-        "alt": "Gallery 15"
-    },
-    {
-        "id": 16,
-        "img": "https://i.imgur.com/NcJd8jD.jpeg",
-        "alt": "Gallery 16"
-    },
-    {
-        "id": 17,
-        "img": "https://i.imgur.com/Rd0QflU.jpeg",
-        "alt": "Gallery 17"
-    },
-    {
-        "id": 18,
-        "img": "https://i.imgur.com/Zggpo99.jpeg",
-        "alt": "Gallery 18"
-    },
-    {
-        "id": 19,
-        "img": "https://i.imgur.com/ZGvkqC5.jpeg",
-        "alt": "Gallery 19"
-    },
-    {
-        "id": 20,
-        "img": "https://i.imgur.com/NYE92Zn.jpeg",
-        "alt": "Gallery 20"
-    },
-    {
-        "id": 21,
-        "img": "https://i.imgur.com/bBpV8bQ.jpeg",
-        "alt": "Gallery 21"
-    },
-    {
-        "id": 22,
-        "img": "https://i.imgur.com/BOoniXt.jpeg",
-        "alt": "Gallery 22"
-    },
-    {
-        "id": 23,
-        "img": "https://i.imgur.com/uDbsHaB.jpeg",
-        "alt": "Gallery 23"
-    },
-    {
-        "id": 24,
-        "img": "https://i.imgur.com/yvJJwsn.jpeg",
-        "alt": "Gallery 24"
-    },
-    {
-        "id": 25,
-        "img": "https://i.imgur.com/jvoxcqZ.jpeg",
-        "alt": "Gallery 25"
-    },
-    {
-        "id": 26,
-        "img": "https://i.imgur.com/tbPU8jJ.jpeg",
-        "alt": "Gallery 26"
-    },
-    {
-        "id": 27,
-        "img": "https://i.imgur.com/zxB4fnU.jpeg",
-        "alt": "Gallery 27"
-    },
-    {
-        "id": 28,
-        "img": "https://i.imgur.com/Lv7HQrr.jpeg",
-        "alt": "Gallery 28"
-    },
-    {
-        "id": 29,
-        "img": "https://i.imgur.com/I5pwOlT.jpeg",
-        "alt": "Gallery 29"
-    },
-    {
-        "id": 30,
-        "img": "https://i.imgur.com/PayTaDB.jpeg",
-        "alt": "Gallery 30"
-    },
-    {
-        "id": 31,
-        "img": "https://i.imgur.com/NBVXSIb.jpeg",
-        "alt": "Gallery 31"
-    },
-    {
-        "id": 32,
-        "img": "https://i.imgur.com/yuV5LJx.jpeg",
-        "alt": "Gallery 32"
-    },
-    {
-        "id": 33,
-        "img": "https://i.imgur.com/QEUkVoc.jpeg",
-        "alt": "Gallery 33"
-    },
-    {
-        "id": 34,
-        "img": "https://i.imgur.com/6aulZ1G.jpeg",
-        "alt": "Gallery 34"
-    },
-    {
-        "id": 35,
-        "img": "https://i.imgur.com/jpHo6rl.jpeg",
-        "alt": "Gallery 35"
-    }
-]
+    { "id": 1, "img": Gallery1, "alt": "Gallery 1" },
+    { "id": 2, "img": Gallery2, "alt": "Gallery 2" },
+    { "id": 3, "img": Gallery3, "alt": "Gallery 3" },
+    { "id": 4, "img": Gallery4, "alt": "Gallery 4" },
+    { "id": 5, "img": Gallery5, "alt": "Gallery 5" },
+    { "id": 6, "img": Gallery6, "alt": "Gallery 6" },
+    { "id": 7, "img": Gallery7, "alt": "Gallery 7" },
+    { "id": 8, "img": Gallery8, "alt": "Gallery 8" },
+    { "id": 9, "img": Gallery9, "alt": "Gallery 9" },
+    { "id": 10, "img": Gallery10, "alt": "Gallery 10" },
+    { "id": 11, "img": Gallery11, "alt": "Gallery 11" },
+    { "id": 12, "img": Gallery12, "alt": "Gallery 12" },
+    { "id": 13, "img": Gallery13, "alt": "Gallery 13" },
+    { "id": 14, "img": Gallery14, "alt": "Gallery 14" },
+    { "id": 15, "img": Gallery15, "alt": "Gallery 15" },
+    { "id": 16, "img": Gallery16, "alt": "Gallery 16" },
+    { "id": 17, "img": Gallery17, "alt": "Gallery 17" },
+    { "id": 18, "img": Gallery18, "alt": "Gallery 18" },
+    { "id": 19, "img": Gallery19, "alt": "Gallery 19" },
+    { "id": 20, "img": Gallery20, "alt": "Gallery 20" },
+    { "id": 21, "img": Gallery21, "alt": "Gallery 21" },
+    { "id": 22, "img": Gallery22, "alt": "Gallery 22" },
+    { "id": 23, "img": Gallery23, "alt": "Gallery 23" },
+    { "id": 24, "img": Gallery24, "alt": "Gallery 24" },
+    { "id": 25, "img": Gallery25, "alt": "Gallery 25" },
+    { "id": 26, "img": Gallery26, "alt": "Gallery 26" },
+    { "id": 27, "img": Gallery27, "alt": "Gallery 27" },
+    { "id": 28, "img": Gallery28, "alt": "Gallery 28" },
+    { "id": 29, "img": Gallery29, "alt": "Gallery 29" },
+    { "id": 30, "img": Gallery30, "alt": "Gallery 30" },
+    { "id": 31, "img": Gallery31, "alt": "Gallery 31" },
+    { "id": 32, "img": Gallery32, "alt": "Gallery 32" },
+    { "id": 33, "img": Gallery33, "alt": "Gallery 33" },
+    { "id": 34, "img": Gallery34, "alt": "Gallery 34" },
+    { "id": 35, "img": Gallery35, "alt": "Gallery 35" }
+];
 
 export default GalleryData;
