54c62fa02b3e81dc8052754003a301b705edfddd
RanR112 <146650544+RanR112@users.noreply.github.com>
2025-05-15 18:56:01 +0700

    add mas bagus

--- src/assets/index.js
+++ src/assets/index.js
@@ -1,5 +1,6 @@
 export { default as logo } from './images/logo.png';
 export { default as pakwawan } from './images/pakwawan.jpg';
+export { default as masbagus } from './images/masbagus.jpg';
 export { default as aboutImg } from './images/about.jpg';
 export { default as video_webm } from './videos/Tos.webm';
 export { default as video_mp4 } from './videos/Tos.mp4';
