466149bc5f7f59cd907b3dd6ddaa82d369eb315e
RanR112 <146650544+RanR112@users.noreply.github.com>
2025-07-06 15:13:52 +0700

    add achievements and change mas bagus photo

--- src/global/Achievements.js
+++ src/global/Achievements.js
@@ -8,6 +8,11 @@ const AchievementData = [
     { year: 2023, title: 'FLS2N SMA 2023 Cabang Lomba Baca Puisi tingkat Provinsi (Juara III)' },
     { year: 2024, title: 'FLS2N SMA 2024 Cabang Lomba Monolog tingkat Kabupaten Banjarnegara (Juara I)' },
     { year: 2024, title: 'FLS2N SMA 2024 Cabang Lomba Baca Puisi tingkat Kabupaten Banjarnegara (Juara III)' },
+    {year: 2024, title: 'Penghargaan Tergiat 2 oleh Komunitas Teater Bara (Teater Banjarnegara) masa kepengurusan 2024/2025'},
+    {year: 2025, title: 'Artefac UNS 2025 tingkat Nasional (Penyaji Terbaik 5)'},
+    {year: 2025, title: 'FLS3N SMA/SMK/MA Cabang Lomba Monolog tingkat kabupaten Banjarnegara (Juara II)'},
+    {year: 2025, title: 'FLS3N SMA/SMK/MA Cabang Lomba Vocal Solo Putra tingkat kabupaten Banjarnegara (Juara I)'},
+    {year: 2025, title: 'FLS3N SMA/SMK/MA Cabang Lomba Vocal Solo Putra tingkat Cabang Dinas Wilayah IX (Juara III)'},
 ]
 
 export default AchievementData;
